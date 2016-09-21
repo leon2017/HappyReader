31fec7c1ad30fb2ab170ea57b96e77465aade6e1
leon2017 <lijiawangjun@gmail.com>
2016-09-21 18:20:04 +0800

    主界面的搭建

--- app/src/main/java/com/leonwang/app/chinashop/ui/MainActivity.java
+++ app/src/main/java/com/leonwang/app/chinashop/ui/MainActivity.java
@@ -2,18 +2,14 @@ package com.leonwang.app.chinashop.ui;
 
 import android.os.Bundle;
 import android.os.Message;
-import android.support.v7.widget.AppCompatTextView;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.RadioButton;
-import android.widget.RelativeLayout;
-import android.widget.SeekBar;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.leonwang.app.chinashop.R;
 import com.leonwang.app.chinashop.base.RxAppCompatBaseActivity;
 
@@ -22,31 +18,27 @@ import butterknife.BindView;
 public class MainActivity extends RxAppCompatBaseActivity {
 
 
-    //简单的测试
-    @BindView(R.id.appCompatTextView)
-    AppCompatTextView mAppCompatTextView;
-    @BindView(R.id.button)
-    Button mButton;
-    @BindView(R.id.textView)
-    TextView mTextView;
-    @BindView(R.id.toggleButton)
-    ToggleButton mToggleButton;
-    @BindView(R.id.checkBox)
-    CheckBox mCheckBox;
-    @BindView(R.id.radioButton)
-    RadioButton mRadioButton;
-    @BindView(R.id.progressBar)
-    ProgressBar mProgressBar;
-    @BindView(R.id.seekBar)
-    SeekBar mSeekBar;
-    @BindView(R.id.seekBar2)
-    SeekBar mSeekBar2;
-    @BindView(R.id.switch1)
-    Switch mSwitch1;
-    @BindView(R.id.editText)
-    EditText mEditText;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.app_bar_layout)
+    AppBarLayout mAppBarLayout;
+    @BindView(R.id.layFrame)
+    FrameLayout mLayFrame;
+    @BindView(R.id.bottom_navigation_bar)
+    BottomNavigationBar mBottomNavigationBar;
     @BindView(R.id.activity_main)
-    RelativeLayout mActivityMain;
+    LinearLayout mActivityMain;
+
+
+
+    private String TAB[] = {"资讯", "视频", "知乎", "开发者"};
+    private String TITLE;
+    private int TAB_COLOR[] = {R.color.colorPrimary,
+            R.color.slategray,
+            R.color.goldyellow,
+            R.color.mediumslateblue};
+    private int mToolBarBG = R.color.colorPrimary;
+    private Bundle mSavedInstanceState;
 
     @Override
     protected int getLayoutId() {
@@ -55,12 +47,36 @@ public class MainActivity extends RxAppCompatBaseActivity {
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
+        mSavedInstanceState = savedInstanceState;
+        initNavigationBar();
+    }
+
+    private void initNavigationBar() {
+        mBottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        mBottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
+        mBottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.main_icon_news, TAB[0]).setActiveColorResource(TAB_COLOR[0]))
+                .addItem(new BottomNavigationItem(R.mipmap.main_icon_vedio, TAB[1]).setActiveColorResource(TAB_COLOR[1]))
+                .addItem(new BottomNavigationItem(R.mipmap.main_icon_zhihu, TAB[2]).setActiveColorResource(TAB_COLOR[2]))
+                .addItem(new BottomNavigationItem(R.mipmap.main_icon_code, TAB[3]).setActiveColorResource(TAB_COLOR[3]))
+                .setFirstSelectedPosition(0)
+                .initialise();
+        mBottomNavigationBar.setTabSelectedListener(new MyTabListener());
 
     }
 
     @Override
     protected void initToolBar() {
-
+        TITLE = "资讯";
+        mToolbar.setNavigationContentDescription(TITLE);
+        mToolbar.setTitle(TITLE);
+        mToolbar.setNavigationIcon(R.mipmap.toolbar_logo);
+        setSupportActionBar(mToolbar);
+        ActionBar mActionBar = getSupportActionBar();
+        if (mActionBar != null) {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+            mActionBar.setDisplayUseLogoEnabled(false);
+            mActionBar.setDisplayShowTitleEnabled(true);
+        }
     }
 
     @Override
@@ -73,12 +89,46 @@ public class MainActivity extends RxAppCompatBaseActivity {
 
     }
 
+    //tab切换
+    public class MyTabListener implements BottomNavigationBar.OnTabSelectedListener {
+
+        @Override
+        public void onTabSelected(int position) {
+            TITLE = TAB[position];
+            mToolBarBG = TAB_COLOR[position];
+            switch (position) {
+                case 0://资讯
+                    break;
+                case 1://视频
+                    break;
+                case 2://知乎
+                    break;
+                case 3://开发
+                    break;
+            }
+            updateToolbar();
+        }
+
+        @Override
+        public void onTabUnselected(int position) {
+
+        }
+
+        @Override
+        public void onTabReselected(int position) {
+
+        }
+    }
+
+    private void updateToolbar() {
+        mToolbar.setTitle(TITLE);
+        mToolbar.setBackgroundResource(mToolBarBG);
+        onPostCreate(mSavedInstanceState);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.mTintManager.setTintColor(this.getResources().getColor(mToolBarBG));
+    }
 }
-
-
-
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }*/
